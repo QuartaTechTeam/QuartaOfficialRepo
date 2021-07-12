fefd4eb739d0859b2a3fc56c2d611b1ca3a31c91
jex <rijam23>
2021-07-12 18:14:02 +0800

    Commit another Suicide

--- app/src/main/java/com/example/quarta/Otp.java
+++ app/src/main/java/com/example/quarta/Otp.java
@@ -82,7 +82,7 @@ public class Otp extends AppCompatActivity {
                                     progressBar.setVisibility(View.GONE);
                                     buttonVerify.setVisibility(View.VISIBLE);
                                     if(task.isSuccessful()){
-                                        Intent intent = new Intent(getApplicationContext(),HomeDashBoard.class);
+                                        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
                                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                                         startActivity(intent);
                                     } else {
